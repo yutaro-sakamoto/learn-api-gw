71b2b8f10efd2ce3f5df78c9f44ac267b4b22b8d
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-01-02 03:10:13 +0900

    chore: move iam role settings to learn-api-gw-stack.ts

--- lib/REST/LambdaProxyIntegration/rest-lambda-proxy-integration.ts
+++ lib/REST/LambdaProxyIntegration/rest-lambda-proxy-integration.ts
@@ -3,28 +3,10 @@ import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as logs from "aws-cdk-lib/aws-logs";
-import * as iam from "aws-cdk-lib/aws-iam";
 
 export class RESTLambdaProxyIntegration extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
-
-    const apiGatewayLogsRole = new iam.Role(this, "ApiGatewayLogsRole", {
-      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com"),
-      managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName(
-          "service-role/AmazonAPIGatewayPushToCloudWatchLogs",
-        ),
-      ],
-    });
-
-    const apiGatewayAccount = new apigateway.CfnAccount(
-      this,
-      "ApiGatewayAccount",
-      {
-        cloudWatchRoleArn: apiGatewayLogsRole.roleArn,
-      },
-    );
 
     const lambdaFunc = new NodejsFunction(this, "function", {
       runtime: lambda.Runtime.NODEJS_22_X,
@@ -58,11 +40,5 @@ export class RESTLambdaProxyIntegration extends Construct {
       validateRequestBody: true,
       validateRequestParameters: true,
     });
-
-    // Deploy after IAM role is created
-    const deployment = restApi.latestDeployment;
-    if (deployment) {
-      deployment.node.addDependency(apiGatewayAccount);
-    }
   }
 }
--- lib/learn-api-gw-stack.ts
+++ lib/learn-api-gw-stack.ts
@@ -1,10 +1,38 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { RESTLambdaProxyIntegration } from "./REST/LambdaProxyIntegration/rest-lambda-proxy-integration";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class LearnApiGwStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    new RESTLambdaProxyIntegration(this, "RestLambdaProxyIntegration");
+
+    // Create an IAM role for API Gateway to write logs to CloudWatch Logs
+    const apiGatewayLogsRole = new iam.Role(this, "ApiGatewayLogsRole", {
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com"),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AmazonAPIGatewayPushToCloudWatchLogs",
+        ),
+      ],
+    });
+
+    const apiGatewayAccount = new apigateway.CfnAccount(
+      this,
+      "ApiGatewayAccount",
+      {
+        cloudWatchRoleArn: apiGatewayLogsRole.roleArn,
+      },
+    );
+
+    // Create a REST API with a Lambda proxy integration
+    const restLambdaProxy = new RESTLambdaProxyIntegration(
+      this,
+      "RestLambdaProxyIntegration",
+    );
+
+    // add dependency to ensure the account is created before the API
+    restLambdaProxy.node.addDependency(apiGatewayAccount);
   }
 }
